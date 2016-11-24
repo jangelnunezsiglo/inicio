4c63b6f20aef3bf42397775042f3011942b86959
Jose Angel Nùñez <jnunez@elsiglo.mx>
2016-11-24 13:02:17 -0600

    commit prueba
    desc commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>El Siglo - Acceso</title>
+	<link rel="shortcut icon" href="images/" />		
+    <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+  <div class="wrapper">
+	<form class="form-signin" action="dashboard.php" method="post">       
+		<center><center><img src="images/logo.jpg" alt=""></img></center><br><br> 
+		<input type="text" class="form-control" name="tarjeta" placeholder="Numero Tarjeta (16 Digitos)"  required="" autofocus="" />
+		<input type="password" class="form-control" name="orden" placeholder="No. Cliente" required=""/>      
+		<button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Acceso</button>   
+	</form>
+	<p> © Cía Editora de la Laguna S.A. de C.V.asdasdasd</p>
+  </div>
+  </body>
+</html>
